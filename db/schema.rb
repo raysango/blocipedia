4bf441cd5b145573148edd9396508f76e6f1431f
raysango <remongeorge@live.com>
2015-01-22 06:32:16 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120043446) do
+ActiveRecord::Schema.define(version: 20150122031534) do
 
   create_table "amounts", force: true do |t|
     t.datetime "created_at"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150120043446) do
 
   add_index "collaborators", ["user_id"], name: "index_collaborators_on_user_id"
   add_index "collaborators", ["wiki_id"], name: "index_collaborators_on_wiki_id"
+
 
   create_table "plans", force: true do |t|
     t.string   "title"
